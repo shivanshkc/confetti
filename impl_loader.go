092075603a009fc78235304ccfca3729afd49a69
Shivansh Kuchchal <skuchcha@redhat.com>
2021-12-03 00:56:04 +0530

    Progress on generateJSON.

--- impl_loader.go
+++ impl_loader.go
@@ -17,29 +17,91 @@ func (i *implLoader) Load(target interface{}) error {
 		return errors.New("target must be a struct pointer")
 	}
 
-	// Getting the value out of the struct pointer.
+	// Fill out all options values.
+	i.resolveOptions()
+
+	// Getting the value out of the struct pointer to generate final JSON.
 	structValue := reflect.ValueOf(target).Elem().Interface()
+	result, err := i.generateJSON(structValue)
+	if err != nil {
+		return fmt.Errorf("failed to generate JSON: %w", err)
+	}
+
+	// Finally, unmarshalling into the provided target.
+	if err := json.Unmarshal([]byte(result), target); err != nil {
+		return fmt.Errorf("failed to unmarshal results into target: %w", err)
+	}
+
+	return nil
+}
+
+// generateJSON does TODO.
+func (i *implLoader) generateJSON(structValue interface{}) (string, error) {
+	var result string
 
 	// Creating reflection types for looping over fields.
 	reflectValue := reflect.ValueOf(structValue)
 	reflectType := reflect.TypeOf(structValue)
 
-	// The resolved field values will be stored in this map.
-	results := map[string]interface{}{}
-	for i := 0; i < reflectValue.NumField(); i++ {
-		fmt.Println("Field:", reflectType.Field(i))
+	for ind := 0; ind < reflectValue.NumField(); ind++ {
+		fieldReflectValue := reflectValue.Field(ind)
+		fieldReflectType := reflectType.Field(ind)
+
+		// If field is of pointer type, we extract the pointer's value.
+		// This allows for cleaner code below.
+		if fieldReflectValue.Kind() == reflect.Ptr {
+			fieldReflectValue = fieldReflectValue.Elem()
+		}
+
+		// Resolving the value of the struct field.
+		resolvedValue, err := i.resolveField(fieldReflectType)
+		if err != nil {
+			return "", fmt.Errorf("failed to resolve field: %w", err)
+		}
+
+		// If the resolved value is a string, it should be covered
+		// in additional quotes to result in a valid JSON.
+		switch asserted := resolvedValue.(type) {
+		case string:
+			resolvedValue = fmt.Sprintf(`"%s"`, asserted)
+		}
+
+		// If the field is of type struct, we will dive into it recursively.
+		if fieldReflectValue.Kind() == reflect.Struct {
+			// Loop over the struct fields again and merge the JSONs.
+		}
+
+		// Appending the results to the JSON string.
+		result = fmt.Sprintf(`%s "%s":%s`, result, fieldReflectType.Name, resolvedValue)
 	}
 
-	// Marshalling the results map into JSON for later unmarshalling.
-	resultJSON, err := json.Marshal(results)
-	if err != nil {
-		return fmt.Errorf("failed to marshal results: %w", err)
+	// Checking for a trailing comma.
+	if result[len(result)-1] == ',' {
+		result = result[:len(result)-1]
 	}
 
-	// Finally, unmarshalling into the provided target.
-	if err := json.Unmarshal(resultJSON, target); err != nil {
-		return fmt.Errorf("failed to unmarshal results into target: %w", err)
+	return fmt.Sprintf("{%s}", result), nil
+}
+
+// resolveOptions does TODO.
+func (i *implLoader) resolveOptions() {
+	if i.opts == nil {
+		i.opts = defaultLoaderOptions
+		return
 	}
 
-	return nil
+	if i.opts.DefTagName == "" {
+		i.opts.DefTagName = defaultLoaderOptions.DefTagName
+	}
+	if i.opts.EnvTagName == "" {
+		i.opts.EnvTagName = defaultLoaderOptions.EnvTagName
+	}
+	if i.opts.ArgTagName == "" {
+		i.opts.ArgTagName = defaultLoaderOptions.ArgTagName
+	}
+}
+
+// resolveField does TODO.
+func (i *implLoader) resolveField(field reflect.StructField) (interface{}, error) {
+	return nil, nil
 }
--- structs.go
+++ structs.go
@@ -1,13 +1,21 @@
 package confetti
 
+// defaultLoaderOptions are used when the user does not provide any.
+var defaultLoaderOptions = &LoaderOptions{
+	DefTagName: "def",
+	EnvTagName: "env",
+	ArgTagName: "arg",
+	UseDotEnv:  false,
+}
+
 // LoaderOptions can be used to customize the ILoader.
 type LoaderOptions struct {
 	// DefTagName can be used to alter the name of the def tag.
-	DefTagName *string
+	DefTagName string
 	// EnvTagName can be used to alter the name of the env tag.
-	EnvTagName *string
+	EnvTagName string
 	// ArgTagName can be used to alter the name of the arg tag.
-	ArgTagName *string
+	ArgTagName string
 	// UseDotEnv controls whether to read data from the .env file.
-	UseDotEnv *bool
+	UseDotEnv bool
 }
