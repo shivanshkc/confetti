2f82624b2dbf74532cdbaf30e5b65e171879c2ef
Shivansh Kuchchal <skuchcha@redhat.com>
2021-12-03 16:47:39 +0530

    Bug fix.

--- aliases.go
+++ aliases.go
@@ -5,10 +5,10 @@ import (
 )
 
 // rsf is a type alias for *reflect.StructField
-type rsf *reflect.StructField
+type rsf = *reflect.StructField
 
 // msi is a type alias for map[string]interface{}
-type msi map[string]interface{}
+type msi = map[string]interface{}
 
 // structFieldAction represents an action that can be performed over a nested struct field.
 // It handles nested fields well because it receives all the parents of the field as well.
